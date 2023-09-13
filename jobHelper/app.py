495e70ff0936b2c6740bf3e345a765a903b301ab
yeil kim <yeil99@naver.com>
2023-09-13 19:12:11 +0900

    app 수정

--- jobHelper/app.py
+++ jobHelper/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='gevent')
+socketio = SocketIO(app)
 
 
 @app.route('/')
