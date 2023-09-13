70ebfff52131480a6c873d7bcae3f560445aca43
yeil kim <yeil99@naver.com>
2023-09-13 19:37:19 +0900

    socket 수정

--- jobHelper/static/JS/chat.js
+++ jobHelper/static/JS/chat.js
@@ -4,30 +4,31 @@ socket.on('connect', function() {
 });
 
 socket.on('client_message', function(message) {
-    let talkDiv = document.getElementById('talkDiv');
-    let divElement = document.createElement("div");
-    divElement.className = "user-talk";
-    // divElement.innerHTML = message.message;
-    divElement.textContent = message.message;
-    talkDiv.appendChild(divElement);
-    socket.emit('server_message');
+        console.log("client_message: "+message)
+        let talkDiv = document.getElementById('talkDiv');
+        let divElement = document.createElement("div");
+        divElement.className = "user-talk";
+        // divElement.innerHTML = message.message;
+        divElement.textContent = message.message;
+        talkDiv.appendChild(divElement);
+        socket.emit('server_message');
 });
 
  socket.on('server_message', function() {
-            // 서버로부터 받은 길이 정보를 동적으로 생성한 엘리먼트로 추가
-            let talkDiv = document.getElementById('talkDiv');
-            let divElement = document.createElement("div");
-            divElement.className = 'bot-talk';
-            divElement.textContent = Math.random().toFixed(3);
-            talkDiv.appendChild(divElement);
+        console.log("server_message: "+message)
+        let talkDiv = document.getElementById('talkDiv');
+        let divElement = document.createElement("div");
+        divElement.className = 'bot-talk';
+        divElement.textContent = Math.random().toFixed(3);
+        talkDiv.appendChild(divElement);
         });
 
  document.getElementById('sendBtn').addEventListener('click', function(event) {
         event.preventDefault(); // 기본 동작 막기
         let message = document.getElementById('chatMessage').value;
+        console.log("send: "+message)
         socket.emit('client_message', message);
         document.getElementById('chatMessage').value = '';
-        console.log("send: "+message)
  });
 
  function sendMessage() {
