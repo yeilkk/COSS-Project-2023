4c057670ca6c834a18d7ea2cbef110f77101ce30
yeil kim <yeil99@naver.com>
2023-09-13 20:53:23 +0900

    수정

--- jobHelper/static/JS/chat.js
+++ jobHelper/static/JS/chat.js
@@ -6,19 +6,9 @@ socket.on('connect', function() {
 socket.on('client_message', function(message) {
         console.log("client_message: "+message)
         let talkDiv = document.getElementById('talkDiv');
-        let userBubble = document.createElement("div");
-        userBubble.className = "user-bubble";
         let divElement = document.createElement("div");
         divElement.className = "user-talk";
-
-        // let content = document.createElement("a");
-        // content.innerHTML = message;
         divElement.innerHTML = message;
-
-        // divElement.appendChild(content);
-        // talkDiv.appendChild(divElement);
-        // userBubble.appendChild(divElement);
-        // talkDiv.appendChild(userBubble);
         talkDiv.appendChild(divElement);
         socket.emit('server_message');
 });
@@ -26,20 +16,9 @@ socket.on('client_message', function(message) {
  socket.on('server_message', function() {
         console.log("server_message: response")
         let talkDiv = document.getElementById('talkDiv');
-        let botBubble = document.createElement("div");
-        botBubble.className = "bot-bubble";
         let divElement = document.createElement("div");
         divElement.className = 'bot-talk';
-
-        // let content = document.createElement("a");
-        // content.innerHTML = Math.random().toFixed(3);
-
-        divElement.innerHTML = Math.random().toFixed(3);
-        // divElement.innerHTML = Math.random().toFixed(3);
-
-        // divElement.appendChild(content);
-        // botBubble.appendChild(divElement);
-        // talkDiv.appendChild(botBubble);
+        divElement.innerHTML = "RESPONSE";
         talkDiv.appendChild(divElement);
         });
 
@@ -57,18 +36,3 @@ socket.on('client_message', function(message) {
     document.getElementById('chatMessage').value = '';
     console.log("send: "+message)
 }
-
-
-// socket.on('connect', function() {
-//     // var serverButton = document.getElementById('server_button');
-//     // serverButton.addEventListener('click', function() {
-//     //     socket.emit('server_message');
-//     // });
-//
-//     var clientButton = document.getElementById('client_button');
-//     clientButton.addEventListener('click', function() {
-//         var clientMessage = document.getElementById('client_message').value;
-//         socket.emit('client_message', clientMessage);
-//         document.getElementById('client_message').value = '';
-//     });
-// });
